99f4e5e6678cd48327d77459f7a9b9210a8deaee
Tino <dil.tino@gmail.com>
2021-12-29 18:43:12 +0100

    zmena

--- backup.sh
+++ backup.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
 ##############################################################################
-#									     	#
-#  Crontab configuration:  00 12 * * * /home/tino/Desktop/uloha/backup.sh	#
-#									     	#
+#									     									 #
+#  Crontab configuration:  00 12 * * * /home/tino/Desktop/uloha/backup.sh	 #
+#									     									 #
 ##############################################################################
 
 #vars
-home_dir=/home/tino/Desktop
-dest="$home_dir/example"
-filename="backup_`date +%Y``date +%m``date +%d`.tar.gz";
-
-
-# backup of what files
-# backup_files="/home/tino/Desktop/uloha"
-
-#read from cfg file
-#source $home_dir/script/folder_set.config
+bak_dir=/home/tino/Desktop/example
+ssh_path=/home/tino/.ssh
+filename="backup_`date +%Y``date +%m``date +%d`.tar.gz"
+#dump=backup.sql.`date +"%Y%m%d"`
+#user=user
+#pass=password
 
 #compress of file and move it to folder where it is going to be stored
-tar cvf $home_dir/example/$filename $home_dir/uloha/ -T folder_set.config
-
-#move to  backup folder
-cd $home_dir/example
-
-#list the content
-ls
+tar cvf $bak_dir/$filename -T folder_set.config
 
 #send on remote host - prerekvizita bola vygenerovanie ssh klucov, ktore boli nasledne vymenene medzi hostami/servermi
-rsync -av -e "ssh -i $HOME/.ssh/my_key" $home_dir/example/$filename tino@192.168.100.4:$home_dir/sent
+rsync -av -e "ssh -i $ssh_path/test_key" $bak_dir/$filename tino@192.168.100.4:$home_dir/sent
 
 #find and remove files older than 3 days
-find "$home_dir/example" -mtime +3 -type f -exec rm -fv {} \;
+find "$bak_dir" -mtime +3 -type f -exec rm -fv {} \;
+
+#backup from the local machine (use --all-databases for backuping every db)
+#mysqldump --username=${user} --password=${pass} --single-transaction --quick --lock-tables=false my_db | gzip > ${dump}.gz
