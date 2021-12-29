dea12bb225e029a8e293cdedc7b84e73f4b56281
TentYx <73161410+TentYx@users.noreply.github.com>
2021-12-29 15:40:43 +0100

    Add files via upload
    upraveny script

--- backup.sh
+++ backup.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-#find and remove files older than 3 days
-find "/home/tino/Desktop/example" -mtime +3 -type f -exec rm -fv {} \;
+##############################################################################
+#									     	#
+#  Crontab configuration:  00 12 * * * /home/tino/Desktop/uloha/backup.sh	#
+#									     	#
+##############################################################################
+
+#vars
+home_dir=/home/tino/Desktop
+dest="$home_dir/example"
+filename="backup_`date +%Y``date +%m``date +%d`.tar.gz";
+
 
 # backup of what files
 # backup_files="/home/tino/Desktop/uloha"
 
 #read from cfg file
-source /home/tino/Desktop/script/folder_set.config
-
-
-#where it is going to be stored
-dest="/home/tino/Desktop/example"
-
-#name of archive
-filename="backup_`date +%Y``date +%m``date +%d`.tar.gz";
+source $home_dir/script/folder_set.config
 
 #compress of file and move to folder where it is going to be stored
-tar cvf /home/tino/Desktop/example/$filename /home/tino/Desktop/uloha/
+tar cvf $home_dir/example/$filename $home_dir/uloha/
 
 #move to  backup folder
-cd /home/tino/Desktop/example
+cd $home_dir/example
 
 #list the content
 ls
 
-#show disk usage of backup
-du -sh
-
 #send on remote host - prerekvizita bola vygenerovanie ssh klucov, ktore boli nasledne vymenene medzi hostami/servermi
-rsync -a /home/tino/Desktop/example/$filename tino@192.168.100.4:/home/tino/Desktop/sent
+rsync -av -e "ssh -i $HOME/.ssh/my_key" $home_dir/example/$filename tino@192.168.100.4:$home_dir/sent
+
+#find and remove files older than 3 days
+find "$home_dir/example" -mtime +3 -type f -exec rm -fv {} \;
